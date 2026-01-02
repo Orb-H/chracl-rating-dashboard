4950ae4f5db0fc5387e46d935fceb29edff347bb
Jongryeol Han <orb.cov@gmail.com>
2026-01-03 02:42:06 +0900

    [#5] Install biome as a formatter and linter (#6)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,12 +7,7 @@ const eslintConfig = [
   // Override default ignores of eslint-config-next.
   {
     // Default ignores of eslint-config-next:
-    ignores: [
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
+    ignores: [".next/**", "out/**", "build/**", "next-env.d.ts"],
   },
 ];
 
