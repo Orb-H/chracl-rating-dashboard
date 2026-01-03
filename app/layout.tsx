bdc97342e8ab8dcbe0b9b764b15725929e1b22be
Jongryeol Han <orb.cov@gmail.com>
2026-01-04 00:03:33 +0900

    [fix/#20] Move SidebarProvider to layout (#21)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
+import { MySidebar } from "@/components/MySidebar";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -25,7 +27,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.variable} ${geistMono.variable} antialiased`}>
-        {children}
+        <SidebarProvider>
+          <MySidebar />
+          <SidebarInset>{children}</SidebarInset>
+        </SidebarProvider>
       </body>
     </html>
   );
