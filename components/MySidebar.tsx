d2d7e9f50a6e1164cb1913d13df2ade16a6d70eb
Orb-H <orb.cov@gmail.com>
2026-01-04 21:28:52 +0900

    apply active for competitions page

--- components/MySidebar.tsx
+++ components/MySidebar.tsx
@@ -77,7 +77,8 @@ export function MySidebar() {
               </SidebarMenuItem>
               <SidebarMenuItem>
                 {/* TODO(#9): Add collapsible to show competitions */}
-                <SidebarMenuButton asChild>
+                <SidebarMenuButton asChild isActive={isActive("/competitions")}>
+                  {/* TODO(#14): Enable link to the real page when ready */}
                   <Link href="#">
                     <HistoryIcon className="inline" />{" "}
                     <span>대회 목록 (준비중)</span>
