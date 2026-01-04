1c7e981a048733133377878b8579c124dff093c8
Orb-H <orb.cov@gmail.com>
2026-01-04 21:46:38 +0900

    apply code review

--- app/competitions/page.tsx
+++ app/competitions/page.tsx
@@ -7,7 +7,7 @@ import {
   EmptyTitle,
 } from "@/components/ui/empty";
 
-export default function Leaderboard() {
+export default function Competitions() {
   return (
     <main className="min-h-screen w-full max-w-3xl items-center py-32 px-16 bg-white dark:bg-black sm:items-start">
       <Empty>
