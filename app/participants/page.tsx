c5ddd51b60d3f8fe6ae069495e9182b9805f7005
Orb-H <orb.cov@gmail.com>
2026-01-04 20:43:20 +0900

    add participants page with empty content

---
+++ app/participants/page.tsx
@@ -0,0 +1,5 @@
+export default function Participants() {
+  return (
+    <main className="min-h-screen w-full max-w-3xl items-center py-32 px-16 bg-white dark:bg-black sm:items-start"></main>
+  );
+}
