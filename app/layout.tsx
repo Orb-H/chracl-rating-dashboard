db27f2ba36d5b22ad7b70dd3a649bacd4ffb9459
Orb-H <orb.cov@gmail.com>
2026-01-06 00:46:43 +0900

    add header on layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
+import { Header } from "@/components/Header";
 import { MySidebar } from "@/components/MySidebar";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 
@@ -29,7 +30,10 @@ export default function RootLayout({
       <body className={`${inter.variable} ${geistMono.variable} antialiased`}>
         <SidebarProvider>
           <MySidebar />
-          <SidebarInset>{children}</SidebarInset>
+          <SidebarInset>
+            <Header />
+            {children}
+          </SidebarInset>
         </SidebarProvider>
       </body>
     </html>
