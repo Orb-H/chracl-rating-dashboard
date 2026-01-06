00fc03fb12b6d12d566c94c368bf0efd17543332
Orb-H <orb.cov@gmail.com>
2026-01-07 01:14:52 +0900

    apply code review

--- app/leaderboard/page.tsx
+++ app/leaderboard/page.tsx
@@ -2,6 +2,7 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import {
   Table,
   TableBody,
+  TableCaption,
   TableCell,
   TableHead,
   TableHeader,
@@ -10,7 +11,7 @@ import {
 import { loadCurrentRating } from "@/lib/loadCurrentRating";
 import { loadParticipantsById } from "@/lib/loadParticipants";
 
-const columns = ["이름", "레이싱 넘버", "레이팅", "티어", "평균"];
+const columns = ["순위", "이름", "티어", "레이팅", "μ값"];
 // TODO: Set current season as a global constant.
 const currentSeason = "season3";
 
@@ -18,18 +19,18 @@ export default function Leaderboard() {
   const currentRating = loadCurrentRating();
   const participants = loadParticipantsById();
 
-  const participantsWithCurrentRating = Object.entries(participants).map(
-    ([key, participant]) => {
+  const participantsWithCurrentRating = Object.entries(participants)
+    .map(([key, participant]) => {
       return {
         ...participant,
-        ...currentRating[key],
+        ...(currentRating[key] ?? {}),
       };
-    },
-  );
-  participantsWithCurrentRating.sort((a, b) => b.value - a.value);
+    })
+    .slice()
+    .sort((a, b) => b.value - a.value);
 
   return (
-    <div className="min-h-screen w-full items-center py-16 px-8 bg-white dark:bg-black sm:items-start">
+    <main className="flex flex-col min-h-screen w-full max-w-3xl mx-auto items-center py-32 px-16 bg-white dark:bg-black sm:items-start">
       <header className="mb-8 w-full">
         <h1 className="text-4xl font-bold">레이팅 리더보드</h1>
         <p className="mt-2 text-lg text-muted-foreground">
@@ -37,7 +38,7 @@ export default function Leaderboard() {
         </p>
       </header>
       {/* TODO(#40): Add a point graph to map participant by rating value */}
-      {/* TODO(#40): Add a popup component to describe about what "티어" and "평균" is */}
+      {/* TODO(#40): Add a popup component to describe about what "티어", "레이팅", and "μ값" is */}
       <Table>
         <TableHeader>
           <TableRow>
@@ -49,8 +50,11 @@ export default function Leaderboard() {
         <TableBody>
           {/* TODO(#40): Make each row to direct to participant detail page */}
           {/* TODO(#40): Change color palette of the row by tier value */}
-          {participantsWithCurrentRating.map((participant) => (
+          {participantsWithCurrentRating.map((participant, i) => (
             <TableRow key={participant.id}>
+              <TableCell className="font-mono">
+                {participant.value ? i + 1 : "-"}
+              </TableCell>
               <TableCell>
                 <Avatar className="inline-block align-middle">
                   <AvatarImage
@@ -58,29 +62,31 @@ export default function Leaderboard() {
                     alt={participant.displayName}
                     className="object-cover"
                   />
-                  <AvatarFallback>{participant.displayName[0]}</AvatarFallback>
+                  <AvatarFallback>
+                    {participant.displayName?.[0] || ""}
+                  </AvatarFallback>
                 </Avatar>
                 <span className="ml-2 inline-block align-middle">
                   {participant.displayName}
                 </span>
               </TableCell>
-              <TableCell className="font-mono">
-                #{participant.racingNumber}
-              </TableCell>
-              <TableCell className="font-mono">
-                {roundToTwoDecimals(participant.value)}
-              </TableCell>
               <TableCell>
                 {participant.tiers ? participant.tiers[currentSeason] : "-"}
               </TableCell>
               <TableCell className="font-mono">
-                {roundToTwoDecimals(participant.mu)}
+                {participant.value
+                  ? roundToTwoDecimals(participant.value)
+                  : "-"}
+              </TableCell>
+              <TableCell className="font-mono">
+                {participant.mu ? roundToTwoDecimals(participant.mu) : "-"}
               </TableCell>
             </TableRow>
           ))}
         </TableBody>
+        <TableCaption>현재 기준 레이팅 순위</TableCaption>
       </Table>
-    </div>
+    </main>
   );
 }
 
