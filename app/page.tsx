68e2a069d89485b46f113262a50e8b83e885bfc1
Orb-H <orb.cov@gmail.com>
2026-01-14 02:24:23 +0900

    apply code review

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,4 @@
-import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-} from "@/components/ui/card";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
 
 export default function Home() {
   return (
