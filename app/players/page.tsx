f30ff5c3721c39c5500bef0917310b1f8b748de3
Orb-H <orb.cov@gmail.com>
2026-01-15 03:35:25 +0900

    show player cards in card layout

--- app/players/page.tsx
+++ app/players/page.tsx
@@ -1,27 +1,43 @@
-import { ConstructionIcon } from "lucide-react";
-import {
-  Empty,
-  EmptyDescription,
-  EmptyHeader,
-  EmptyMedia,
-  EmptyTitle,
-} from "@/components/ui/empty";
+import { loadCurrentRating } from "@/lib/loadCurrentRating";
+import { loadPlayers } from "@/lib/loadPlayers";
+import { PlayerCard } from "./playerCard";
+
+// TODO: Set current season as a global constant.
+const currentSeason = "season3";
 
 export default function Participants() {
+  const players = loadPlayers();
+  const ratings = loadCurrentRating();
+
+  const playersWithRating = players
+    .map((player) => ({
+      ...player,
+      ...ratings[player.id],
+    }))
+    // TODO(#10): Provide several sort options
+    .sort((a, b) => {
+      return (
+        (b.tiers?.[currentSeason] ?? "").localeCompare(
+          a.tiers?.[currentSeason] ?? "",
+        ) || b.value! - a.value!
+      );
+    });
+
   return (
-    <main className="min-h-screen w-full max-w-3xl items-center py-16 px-8 md:py-32 md:px-16 bg-background md:items_start">
-      {/* TODO(#10): Fill with concrete content */}
-      <Empty>
-        <EmptyHeader>
-          <EmptyMedia>
-            <ConstructionIcon />
-          </EmptyMedia>
-          <EmptyTitle>준비중입니다.</EmptyTitle>
-          <EmptyDescription>
-            현재 레이아웃 작업 및 데이터 확보 작업을 진행중입니다.
-          </EmptyDescription>
-        </EmptyHeader>
-      </Empty>
+    <main className="flex flex-col min-h-screen w-full max-w-3xl mx-auto items-center py-16 px-8 md:py-32 md:px-16 bg-background md:items_start">
+      <header className="mb-8 w-full">
+        <h1 className="text-4xl font-bold">선수 목록</h1>
+        <p className="mt-2 text-lg text-muted-foreground">
+          치레동에 참가하는 선수들을 확인해보세요.
+        </p>
+      </header>
+      {/* TODO(#10): Provide several sort options */}
+      <div className="grid w-full gap-8 grid-cols-1 md:grid-cols-2">
+        {/* TODO(#14): Add a link to each competition*/}
+        {playersWithRating.map((player) => (
+          <PlayerCard key={player.id} player={player} />
+        ))}
+      </div>
     </main>
   );
 }
