b5584f5d1507a9165bfe71b3fa69d314e6fc6713
Orb-H <orb.cov@gmail.com>
2026-01-15 02:53:31 +0900

    add prefers-color-scheme detection script

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,13 +9,30 @@ export const metadata: Metadata = {
   description: "치레동 참가자들의 실력을 숫자로 확인하세요!",
 };
 
+const ThemeInitializerScript = `
+(() => {
+  if (
+    localStorage.theme === "dark" ||
+    (!("theme" in localStorage) &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches)
+  ) {
+    document.documentElement.classList.add("dark");
+  } else {
+    document.documentElement.classList.remove("dark");
+  }
+})();
+`;
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning={true}>
+      <head>
+        <script dangerouslySetInnerHTML={{ __html: ThemeInitializerScript }} />
+      </head>
       <body className={`antialiased`}>
         <SidebarProvider>
           <MySidebar />
