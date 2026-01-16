4f94479794e906a037a463083d6b7ee04bcebd13
Orb-H <orb.cov@gmail.com>
2026-01-17 03:56:46 +0900

    apply code review

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,10 +5,11 @@ import { useTheme } from "next-themes";
 import { Separator } from "@/components/ui/separator";
 import { SidebarTrigger } from "@/components/ui/sidebar";
 import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Button } from "./ui/button";
 import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
 
 export function Header() {
-  const { theme, setTheme } = useTheme();
+  const { theme, setTheme, systemTheme } = useTheme();
 
   return (
     <header className="flex h-12 p-2 shrink-0 items-center gap-4 border-b sticky top-0 bg-background z-10">
@@ -20,16 +21,21 @@ export function Header() {
       </h1>
       <Separator orientation="vertical" />
       <Popover>
-        <PopoverTrigger className="mr-2" aria-label="테마 변경">
-          <SunMoonIcon className="w-5 h-5" />
+        <PopoverTrigger className="mr-2" aria-label="테마 변경" asChild>
+          <Button variant="ghost" size="icon">
+            <SunMoonIcon className="w-5 h-5" />
+          </Button>
         </PopoverTrigger>
         <PopoverContent align="end" className="w-auto p-0">
-          <Tabs value={theme} className="w-full">
+          <Tabs
+            value={theme ?? systemTheme}
+            className="w-full"
+            onValueChange={(value) => setTheme(value)}
+          >
             <TabsList className="bg-transparent border-0 p-1">
               <TabsTrigger
                 value="light"
                 className="not-data-[state=active]:bg-muted not-data-[state=active]:text-muted-foreground"
-                onClick={() => setTheme("light")}
                 aria-label="라이트 모드"
               >
                 <SunIcon className="w-4 h-4" />
@@ -37,7 +43,6 @@ export function Header() {
               <TabsTrigger
                 value="system"
                 className="not-data-[state=active]:bg-muted not-data-[state=active]:text-muted-foreground"
-                onClick={() => setTheme("system")}
                 aria-label="시스템 설정"
               >
                 <SunMoonIcon className="w-4 h-4" />
@@ -45,7 +50,6 @@ export function Header() {
               <TabsTrigger
                 value="dark"
                 className="not-data-[state=active]:bg-muted not-data-[state=active]:text-muted-foreground"
-                onClick={() => setTheme("dark")}
                 aria-label="다크 모드"
               >
                 <MoonIcon className="w-4 h-4" />
