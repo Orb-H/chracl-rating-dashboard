e9ad26e744eaa46af78872815c8eb6e0e560789c
Jongryeol Han <orb.cov@gmail.com>
2026-01-17 03:58:19 +0900

    [#82] Add theme selection button on header (#83)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { ThemeProvider } from "next-themes";
 import { Header } from "@/components/Header";
 import { MySidebar } from "@/components/MySidebar";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
@@ -9,39 +10,26 @@ export const metadata: Metadata = {
   description: "치레동 참가자들의 실력을 숫자로 확인하세요!",
 };
 
-const ThemeInitializerScript = `
-(() => {
-  if (
-    localStorage.theme === "dark" ||
-    (!("theme" in localStorage) &&
-      window.matchMedia("(prefers-color-scheme: dark)").matches)
-  ) {
-    document.documentElement.classList.add("dark");
-  } else {
-    document.documentElement.classList.remove("dark");
-  }
-})();
-`;
-
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    // The theme initializer mutates document.documentElement.classList
+    // The theme provider mutates document.documentElement.classList
     // before React hydration, which can cause an intentional mismatch on <html>.
     // Keep suppressHydrationWarning on <html> to ignore this specific mismatch.
-    <html lang="en" suppressHydrationWarning={true}>
+    <html lang="en" suppressHydrationWarning>
       <body className={`antialiased`}>
-        <script dangerouslySetInnerHTML={{ __html: ThemeInitializerScript }} />
-        <SidebarProvider>
-          <MySidebar />
-          <SidebarInset>
-            <Header />
-            {children}
-          </SidebarInset>
-        </SidebarProvider>
+        <ThemeProvider defaultTheme="system" enableSystem attribute="class">
+          <SidebarProvider>
+            <MySidebar />
+            <SidebarInset>
+              <Header />
+              {children}
+            </SidebarInset>
+          </SidebarProvider>
+        </ThemeProvider>
       </body>
     </html>
   );
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,15 +1,63 @@
 "use client";
 
+import { MoonIcon, SunIcon, SunMoonIcon } from "lucide-react";
+import { useTheme } from "next-themes";
 import { Separator } from "@/components/ui/separator";
 import { SidebarTrigger } from "@/components/ui/sidebar";
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Button } from "./ui/button";
+import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
 
 export function Header() {
+  const { theme, setTheme, systemTheme } = useTheme();
+
   return (
     <header className="flex h-12 p-2 shrink-0 items-center gap-4 border-b sticky top-0 bg-background z-10">
       <SidebarTrigger className="ml-2" />
       <Separator orientation="vertical" />
       {/* TODO(#9): Add breadcrumb indicating where the user is in the app */}
-      <h1 className="text-lg font-semibold mx-2">치레동 레이팅 대시보드</h1>
+      <h1 className="text-lg font-semibold mx-2 mr-auto">
+        치레동 레이팅 대시보드
+      </h1>
+      <Separator orientation="vertical" />
+      <Popover>
+        <PopoverTrigger className="mr-2" aria-label="테마 변경" asChild>
+          <Button variant="ghost" size="icon">
+            <SunMoonIcon className="w-5 h-5" />
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent align="end" className="w-auto p-0">
+          <Tabs
+            value={theme ?? systemTheme}
+            className="w-full"
+            onValueChange={(value) => setTheme(value)}
+          >
+            <TabsList className="bg-transparent border-0 p-1">
+              <TabsTrigger
+                value="light"
+                className="not-data-[state=active]:bg-muted not-data-[state=active]:text-muted-foreground"
+                aria-label="라이트 모드"
+              >
+                <SunIcon className="w-4 h-4" />
+              </TabsTrigger>
+              <TabsTrigger
+                value="system"
+                className="not-data-[state=active]:bg-muted not-data-[state=active]:text-muted-foreground"
+                aria-label="시스템 설정"
+              >
+                <SunMoonIcon className="w-4 h-4" />
+              </TabsTrigger>
+              <TabsTrigger
+                value="dark"
+                className="not-data-[state=active]:bg-muted not-data-[state=active]:text-muted-foreground"
+                aria-label="다크 모드"
+              >
+                <MoonIcon className="w-4 h-4" />
+              </TabsTrigger>
+            </TabsList>
+          </Tabs>
+        </PopoverContent>
+      </Popover>
     </header>
   );
 }
