5dfe08dd0dcb456052f73bd8a040f93ab26e937a
Orb-H <orb.cov@gmail.com>
2026-01-18 21:40:24 +0900

    use gap instead

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -18,17 +18,17 @@ export default function NotFound() {
         URL을 다시 한 번 확인해 주세요. 혹시라도 있어야 하는 페이지인데 존재하지
         않는다면, 현재 주소와 함께 제작자에게 메일을 보내주세요.
       </p>
-      <div className="flex flex-col md:flex-row my-10">
-        <Button className="md:mx-2 my-2 md:my-0" onClick={() => router.back()}>
+      <div className="flex flex-col md:flex-row my-10 gap-2">
+        <Button onClick={() => router.back()}>
           <Undo2Icon /> 이전 페이지로 돌아가기
         </Button>
-        <Button variant="secondary" className="md:mx-2 my-2 md:my-0" asChild>
+        <Button variant="secondary" asChild>
           <Link href="/">
             <HomeIcon />
             홈으로 돌아가기
           </Link>
         </Button>
-        <Button variant="ghost" className="md:mx-2 my-2 md:my-0" asChild>
+        <Button variant="ghost" asChild>
           <Link href="mailto:orbithv@orbithv.dev">
             <MailIcon /> 제작자에게 메일 보내기
           </Link>
