e2a8bfd0d5fd30dac1cd6f2111ba2b1c6110e398
Orb-H <orb.cov@gmail.com>
2026-01-18 21:27:46 +0900

    apply code review

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -19,16 +19,16 @@ export default function NotFound() {
         않는다면, 현재 주소와 함께 제작자에게 메일을 보내주세요.
       </p>
       <div className="flex flex-col md:flex-row my-10">
-        <Button className="mx-2 my-2" onClick={() => router.back()}>
+        <Button className="md:mx-2 my-2 md:my-0" onClick={() => router.back()}>
           <Undo2Icon /> 이전 페이지로 돌아가기
         </Button>
-        <Button variant="secondary" className="mx-2 my-2" asChild>
+        <Button variant="secondary" className="md:mx-2 my-2 md:my-0" asChild>
           <Link href="/">
             <HomeIcon />
             홈으로 돌아가기
           </Link>
         </Button>
-        <Button variant="ghost" className="mx-2 my-2" asChild>
+        <Button variant="ghost" className="md:mx-2 my-2 md:my-0" asChild>
           <Link href="mailto:orbithv@orbithv.dev">
             <MailIcon /> 제작자에게 메일 보내기
           </Link>
