b0283b592912132acc88eb5bf32fc6e94d438a25
Orb-H <orb.cov@gmail.com>
2026-01-19 23:51:10 +0900

    replace participant with player

--- app/players/[id]/page.tsx
+++ app/players/[id]/page.tsx
@@ -8,20 +8,20 @@ import { RatingChart } from "./RatingChart";
 export const dynamicParams = false;
 
 export async function generateStaticParams() {
-  const participants = loadPlayers();
+  const players = loadPlayers();
 
-  return participants.map((participant) => ({
-    id: participant.id,
+  return players.map((player) => ({
+    id: player.id,
   }));
 }
 
-export default async function Participant({
+export default async function Player({
   params,
 }: {
   params: Promise<{ id: string }>;
 }) {
   const id = (await params).id;
-  const participant = (() => {
+  const player = (() => {
     try {
       return loadPlayerById(id);
     } catch {
@@ -78,7 +78,7 @@ export default async function Participant({
 
   return (
     <main className="flex flex-col min-h-screen w-full max-w-3xl mx-auto items-center py-16 px-8 md:py-32 md:px-16 bg-background md:items-start">
-      <h1 className="mb-8 text-4xl font-bold">{participant.displayName}</h1>
+      <h1 className="mb-8 text-4xl font-bold">{player.displayName}</h1>
       <RatingChart ratingHistoryByMatch={ratingHistoryByMatch} />
     </main>
   );
