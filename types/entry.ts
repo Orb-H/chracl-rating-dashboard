373598c0cac6e99af452f2ef69dbb8a6af7db882
Orb-H <orb.cov@gmail.com>
2026-01-19 22:31:28 +0900

    edit entry type

--- types/entry.ts
+++ types/entry.ts
@@ -1,10 +1,26 @@
 export type Entry = {
-  // The unique identifier for the entry. E.g., "season1-round1-qual"
+  /**
+   *  The unique identifier for the entry.
+   * E.g., "season1-round1-qual"
+   */
   id: string;
 
-  // The arbitrary number for sorting entries. E.g., 1010100
+  /**
+   * The competition ID this entry belongs to.
+   * This value is empty only if the entry is the initial state.
+   * E.g., "season1"
+   */
+  competitionId?: string;
+
+  /**
+   * The arbitrary number for sorting entries.
+   * E.g., 1010100
+   */
   sortKey: number;
 
-  // Additional note about the entry. E.g., "This is the qualifying round for Season 1, Round 1."
+  /**
+   * Additional note about the entry.
+   * E.g., "This is the qualifying round for Season 1, Round 1."
+   */
   note: string;
 };
