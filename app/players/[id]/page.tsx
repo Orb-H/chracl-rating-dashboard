0bad798e611ff1c7c758aa5c69e548da19c9fa60
Jongryeol Han <orb.cov@gmail.com>
2026-01-20 01:02:18 +0900

    [#11] Implement basic separated layout of player detail page (#104)

--- app/players/[id]/page.tsx
+++ app/players/[id]/page.tsx
@@ -3,6 +3,7 @@ import { loadEntriesById } from "@/lib/loadEntries";
 import { loadHistoriesById } from "@/lib/loadHistories";
 import { loadMatches } from "@/lib/loadMatches";
 import { loadPlayerById, loadPlayers } from "@/lib/loadPlayers";
+import { CollapsibleItem } from "./CollapsibleItem";
 import { RatingChart } from "./RatingChart";
 
 export const dynamicParams = false;
@@ -74,7 +75,13 @@ export default async function Player({
   return (
     <main className="flex flex-col min-h-screen w-full max-w-3xl mx-auto items-center py-16 px-8 md:py-32 md:px-16 bg-background md:items-start">
       <h1 className="mb-8 text-4xl font-bold">{player.displayName}</h1>
-      <RatingChart ratingHistoryByMatch={ratingHistoryByMatch} />
+      <CollapsibleItem title="선수 프로필" defaultOpen>
+        {/* TODO(#11): Add a short content about brief profile */}
+        추가 예정입니다.
+      </CollapsibleItem>
+      <CollapsibleItem title="레이팅 그래프">
+        <RatingChart ratingHistoryByMatch={ratingHistoryByMatch} />
+      </CollapsibleItem>
     </main>
   );
 }
