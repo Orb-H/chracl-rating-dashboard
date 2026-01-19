6c1ab6cdaee770563ae03cd3a16e69b2117a41ab
Orb-H <orb.cov@gmail.com>
2026-01-19 23:27:32 +0900

    implement linechart of ratings by matches

--- app/players/[id]/page.tsx
+++ app/players/[id]/page.tsx
@@ -1,4 +1,7 @@
 import { notFound } from "next/navigation";
+import { loadEntriesById } from "@/lib/loadEntries";
+import { loadHistoriesById } from "@/lib/loadHistories";
+import { loadMatches } from "@/lib/loadMatches";
 import { loadPlayerById, loadPlayers } from "@/lib/loadPlayers";
 import { RatingChart } from "./RatingChart";
 
@@ -21,15 +24,62 @@ export default async function Participant({
   const participant = (() => {
     try {
       return loadPlayerById(id);
-    } catch (_) {
+    } catch {
       return notFound();
     }
   })();
+  const entries = (() => {
+    try {
+      return loadEntriesById();
+    } catch (e) {
+      throw new Error(
+        "Failed to load entries" + (e instanceof Error ? ": " + e.message : ""),
+      );
+    }
+  })();
+  const histories = (() => {
+    try {
+      return loadHistoriesById(id).sort((a, b) => {
+        return entries[a.entryId].sortKey - entries[b.entryId].sortKey;
+      });
+    } catch (e) {
+      throw new Error(
+        "Failed to load histories" +
+          (e instanceof Error ? ": " + e.message : ""),
+      );
+    }
+  })();
+  const participatedMatches = (() => {
+    try {
+      return loadMatches().filter((match) =>
+        match.participants.some((p) => p.id === id),
+      );
+    } catch (e) {
+      throw new Error(
+        "Failed to load matches" + (e instanceof Error ? ": " + e.message : ""),
+      );
+    }
+  })();
+
+  const ratingHistoryByMatch = histories.map((history) => {
+    try {
+      const match = participatedMatches.find(
+        (match) => match.entryId === history.entryId,
+      );
+      return {
+        ...history,
+        ...match,
+        name: match ? match.competitionId + " " + match.name : "",
+      };
+    } catch {
+      throw new Error("Failed to load match for history");
+    }
+  });
 
   return (
     <main className="flex flex-col min-h-screen w-full max-w-3xl mx-auto items-center py-16 px-8 md:py-32 md:px-16 bg-background md:items-start">
       <h1 className="mb-8 text-4xl font-bold">{participant.displayName}</h1>
-      <RatingChart />
+      <RatingChart ratingHistoryByMatch={ratingHistoryByMatch} />
     </main>
   );
 }
