79b168bb67ccd37a98de056fca4d7734d04026b8
Jongryeol Han <orb.cov@gmail.com>
2026-01-22 07:02:34 +0900

    [#108] Introduce shortName in competition and shorten some competitions in data (#113)
    ## Background
    
    - Issue: #108
    - Resolves #108
    
    ## Changes
    
    - Introduce shortName in `competition` type
    - Add shortName in competition data
    - Shorten competition-related variables in data

--- types/competition.ts
+++ types/competition.ts
@@ -1,27 +1,55 @@
 export type Competition = {
-  // The unique identifier for the competition. E.g., "season1"
+  /**
+   * The unique identifier for the competition.
+   * E.g., "season1"
+   */
   id: string;
 
-  // The display name of the competition. E.g., "제 1회 치지직 레이싱 동아리: 고속도로 배틀"
+  /**
+   * The display name of the competition.
+   * E.g., "제 1회 치지직 레이싱 동아리: 고속도로 배틀"
+   */
   name: string;
 
-  // The sort order of the competition. Lower numbers mean older competitions. E.g., 1
+  /**
+   * The short name of the competition.
+   * E.g., "제 1회: 고속도로 배틀"
+   */
+  shortName?: string;
+
+  /**
+   * The sort order of the competition. Lower numbers mean older competitions.
+   * E.g., 1
+   */
   sortOrder: number;
 
-  // The list of teams participating in the competition.
+  /**
+   * The list of teams participating in the competition.
+   */
   teams: Team[];
 
-  // The scoring scheme for the competition. E.g., { "race1": [10, 8, 6, 5, 4, 3, 2, 1], "race2": [15, 12, 10, 8, 6, 4, 2, 1] }
+  /**
+   * The scoring scheme for the competition. E.g., { "race1": [10, 8, 6, 5, 4, 3, 2, 1], "race2": [15, 12, 10, 8, 6, 4, 2, 1] }
+   */
   pointsSchemes: Record<string, number[]>;
 };
 
 export type Team = {
-  // The unique identifier for the team. E.g., "season1-team-a"
+  /**
+   * The unique identifier for the team.
+   * E.g., "season1-team-a"
+   */
   id: string;
 
-  // The display name of the team. E.g., "Team A"
+  /**
+   * The display name of the team.
+   * E.g., "Team A"
+   */
   name: string;
 
-  // The list of team member names. E.g., ["Alice", "Bob", "Charlie"]
+  /**
+   * The list of team member names.
+   * E.g., ["Alice", "Bob", "Charlie"]
+   */
   members: string[];
 };
