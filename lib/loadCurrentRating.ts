3e2e833952a3e7dc6d11704594501d8b5e40f519
Orb-H <orb.cov@gmail.com>
2026-03-08 20:41:49 +0900

    use history to get currentRating

--- lib/loadCurrentRating.ts
+++ lib/loadCurrentRating.ts
@@ -1,25 +1,29 @@
-import fs from "fs";
-import path from "path";
 import { Rating } from "@/types/rating";
+import { loadHistoriesById } from "./loadHistories";
+import { loadPlayers } from "./loadPlayers";
 
 let currentRating: Record<string, Rating> | undefined = undefined;
 
 function loadData(): Record<string, Rating> {
-  const dataPath = path.join(process.cwd(), "data", "currentRating.json");
-
-  if (!fs.existsSync(dataPath)) {
-    throw new Error(`Current rating data file not found at path: ${dataPath}`);
-  }
-
   try {
-    const jsonData = fs.readFileSync(dataPath, "utf-8");
-    const parsed = JSON.parse(jsonData);
-    return parsed as Record<string, Rating>;
+    const data: Record<string, Rating> = {};
+    const players = loadPlayers();
+
+    players.forEach((player) => {
+      const histories = loadHistoriesById(player.id);
+      if (histories.length > 1) {
+        // Ignores players with no history or only one history entry (i.e., no rating changes yet)
+        const latestHistory = histories[histories.length - 1];
+        data[player.id] = latestHistory.rating;
+      }
+    });
+
+    return data;
   } catch (err) {
     const message =
       err instanceof Error
-        ? `Failed to load current rating data from ${dataPath}: ${err.message}`
-        : `Failed to load current rating data from ${dataPath}.`;
+        ? `Failed to load current rating data: ${err.message}`
+        : `Failed to load current rating data.`;
     throw new Error(message);
   }
 }
