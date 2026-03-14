893df74508f078708602c3e284d143459add60ee
Jongryeol Han <orb.cov@gmail.com>
2026-03-15 02:26:42 +0900

    [#211] Introduce `Prediction` type for prediction data (#213)

---
+++ lib/loadPredictions.ts
@@ -0,0 +1,76 @@
+import fs from "fs";
+import path from "path";
+import { Prediction } from "@/types/prediction";
+
+let predictions: Prediction[] | undefined = undefined;
+let predictionsByMatchId: Record<string, Prediction> | undefined = undefined;
+
+function loadData(): Prediction[] {
+  const dataDir = path.join(process.cwd(), "data", "predictions");
+
+  if (!fs.existsSync(dataDir)) {
+    throw new Error(`Predictions data directory not found at path: ${dataDir}`);
+  }
+
+  let fileNames: string[];
+  try {
+    fileNames = fs
+      .readdirSync(dataDir)
+      .filter((file) => file.endsWith(".json"));
+  } catch (err) {
+    const message =
+      err instanceof Error
+        ? `Failed to read predictions data directory at ${dataDir}: ${err.message}`
+        : `Failed to read predictions data directory at ${dataDir}.`;
+    throw new Error(message);
+  }
+
+  const predictionsData: Prediction[] = [];
+  for (const fileName of fileNames) {
+    const filePath = path.join(dataDir, fileName);
+    try {
+      const jsonData = fs.readFileSync(filePath, "utf-8");
+      predictionsData.push(JSON.parse(jsonData) as Prediction);
+    } catch (err) {
+      const message =
+        err instanceof Error
+          ? `Failed to load predictions data from file ${filePath}: ${err.message}`
+          : `Failed to load predictions data from file ${filePath}.`;
+      throw new Error(message);
+    }
+  }
+
+  return predictionsData;
+}
+
+export function loadPredictions(): Prediction[] {
+  if (predictions === undefined) predictions = loadData();
+  return predictions;
+}
+
+export function loadPredictionsByMatchId(): Record<string, Prediction> {
+  if (predictionsByMatchId === undefined) {
+    const ids = loadPredictions().map(
+      (prediction) => prediction.reference.matchId,
+    );
+    if (ids.length !== new Set(ids).size) {
+      throw new Error(`Duplicate match IDs found in predictions data.`);
+    }
+
+    predictionsByMatchId = Object.fromEntries(
+      loadPredictions().map((prediction) => [
+        prediction.reference.matchId,
+        prediction,
+      ]),
+    );
+  }
+  return predictionsByMatchId;
+}
+
+export function loadPredictionByMatchId(matchId: string): Prediction {
+  const prediction = loadPredictionsByMatchId()[matchId];
+  if (!prediction) {
+    throw new Error(`Prediction not found for match ID: ${matchId}`);
+  }
+  return prediction;
+}
---
+++ types/prediction.ts
@@ -0,0 +1,56 @@
+export type Prediction = {
+  /**
+   * The reference to the match this prediction is for.
+   */
+  reference: PredictionReference;
+
+  /**
+   * The prediction results for individual players participating in this match.
+   */
+  individual: Record<string, IndividualPredictionResult>;
+
+  /**
+   * The prediction results for teams participating in this match. This field exists only when the match kind is "TEAM".
+   */
+  team?: Record<string, TeamPredictionResult>;
+};
+
+// TODO(#211): Introduce reference for custom setups.
+export type PredictionReference = ExistingReference;
+
+export type ExistingReference = {
+  /**
+   * The id of the match this prediction is for.
+   * E.g., `season1-round1-qual`
+   */
+  matchId: string;
+};
+
+export type IndividualPredictionResult = {
+  /**
+   * The probability of the player achieving each rank, in percentage.
+   * E.g., [50, 30, 20] means 50% chance for 1st place, 30% chance for 2nd place, and 20% chance for 3rd place.
+   */
+  prob: number[];
+
+  /**
+   * The expected rank of the player, calculated as the weighted average of ranks using the probabilities.
+   * E.g., 1.7 means the expected rank is between 1st and 2nd place, closer to 2nd place.
+   */
+  rank: number;
+
+  /**
+   * The expected point the player would earn from the match, calculated as the weighted average of points using the probabilities.
+   * This value exists only if the points scheme for the match is defined.
+   * E.g., 8.5
+   */
+  point?: number;
+};
+
+export type TeamPredictionResult = {
+  /**
+   * The probability of the team achieving each rank, in percentage.
+   * E.g., [60, 25, 15] means 60% chance for 1st place, 25% chance for 2nd place, and 15% chance for 3rd place.
+   */
+  prob: number[];
+};
