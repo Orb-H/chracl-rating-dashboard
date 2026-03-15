f1f23689530aeece146cc35a0225e34fa6275eb3
Orb-H <orb.cov@gmail.com>
2026-03-16 01:44:17 +0900

    add simple update sheet

--- components/MySidebar.tsx
+++ components/MySidebar.tsx
@@ -25,6 +25,14 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import {
   Sidebar,
   SidebarContent,
   SidebarFooter,
@@ -37,8 +45,10 @@ import {
   SidebarMenuItem,
   useSidebar,
 } from "@/components/ui/sidebar";
+import { Update } from "@/types/update";
+import { Separator } from "./ui/separator";
 
-export function MySidebar() {
+export function MySidebar({ updates }: { updates: Update[] }) {
   const pathname = usePathname();
 
   const isActive = (href: string) => {
@@ -122,14 +132,40 @@ export function MySidebar() {
                   </Link>
                 </SidebarMenuButton>
               </SidebarMenuItem>
-              <SidebarMenuItem>
-                <SidebarMenuButton asChild>
-                  <Link href="#">
-                    <ScrollTextIcon className="inline" />{" "}
-                    <span>업데이트 내역 (준비중)</span>
-                  </Link>
-                </SidebarMenuButton>
-              </SidebarMenuItem>
+              <Sheet>
+                <SheetTrigger asChild>
+                  <SidebarMenuItem>
+                    <SidebarMenuButton asChild>
+                      <div>
+                        <ScrollTextIcon className="inline" />{" "}
+                        <span>업데이트 내역 (준비중)</span>
+                      </div>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                </SheetTrigger>
+                <SheetContent side="left">
+                  <SheetHeader>
+                    <SheetTitle>업데이트 내역</SheetTitle>
+                    <SheetDescription>
+                      업데이트 날짜는 배포 날짜가 아닌 깃허브에 기능이 추가된
+                      날짜를 기준으로 합니다. 따라서 실제로 페이지가 배포되지
+                      않은 날짜에 개발이 완료되었다고 표시된 기능이 있을 수
+                      있습니다.
+                    </SheetDescription>
+                  </SheetHeader>
+                  {updates.map((update) => (
+                    <div key={update.date} className="px-4">
+                      <Separator className="my-4" />
+                      <h3 className="font-semibold mb-4">{update.date}</h3>
+                      <ul className="text-sm list-disc list-inside">
+                        {update.content.map((item, index) => (
+                          <li key={index}>{item}</li>
+                        ))}
+                      </ul>
+                    </div>
+                  ))}
+                </SheetContent>
+              </Sheet>
             </SidebarMenu>
           </SidebarGroupContent>
         </SidebarGroup>
